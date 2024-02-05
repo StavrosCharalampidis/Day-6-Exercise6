039ea73bda3afe59c585fbf277534bfa14931cd8
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-05 13:49:43 +0200

    (Try the f-string and strings) Complete

---
+++ ex6.py
@@ -0,0 +1,20 @@
+# The Variable x store a String
+x = "This is X"
+
+# The Variable y store a String and the Variable x
+y = f'This is y, who has a x {x}! '
+
+# The Variable i store a String
+i = "First and "
+
+# The Variable j store a String
+j = "Seccond Combine {}"
+
+# Print The x 
+print(x)
+
+# Print the y 
+print(y)
+
+# Print the Combine String with the format
+print((i + j).format(True))
